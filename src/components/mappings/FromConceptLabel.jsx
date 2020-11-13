4078695e54f810b741c3c8aa0fa04dd607ce674c
Sny <sny.aggarwal@gmail.com>
2020-11-13 18:06:32 +0530

    Mapping labels to concept links

--- src/components/mappings/FromConceptLabel.jsx
+++ src/components/mappings/FromConceptLabel.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import {LocalOffer as LocalOfferIcon} from '@material-ui/icons'
 import ResourceLabel from '../common/ResourceLabel';
 import { DARKGRAY } from '../../common/constants';
 
 const FromConceptLabel = props => {
+  const labelComponent = <ResourceLabel
+                           owner={props.from_source_owner}
+                           parent={props.from_source_name}
+                           id={props.from_concept_code}
+                           name={props.from_concept_name}
+                           icon={
+                             <LocalOfferIcon
+                               fontSize='small'
+                                         style={{width: '10pt', color: DARKGRAY}}
+                             />
+                           } />;
   return (
-    <ResourceLabel
-      owner={props.from_source_owner} parent={props.from_source_name}
-      id={props.from_concept_code} name={props.from_concept_name}
-      icon={<LocalOfferIcon fontSize='small' style={{width: '10pt', color: DARKGRAY}}/>}
-    />
+    <React.Fragment>
+      {
+        props.from_concept_url ?
+        <Link to={props.from_concept_url}>{labelComponent}</Link>:
+        <span>{labelComponent}</span>
+      }
+    </React.Fragment>
   )
 }
 export default FromConceptLabel;
--- src/components/mappings/ToConceptLabel.jsx
+++ src/components/mappings/ToConceptLabel.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import {LocalOffer as LocalOfferIcon} from '@material-ui/icons'
 import ResourceLabel from '../common/ResourceLabel';
 import { DARKGRAY } from '../../common/constants';
 
 const ToConceptLabel = props => {
+  const labelComponent = <ResourceLabel
+                           owner={props.to_source_owner}
+                           parent={props.to_source_name}
+                           id={props.to_concept_code}
+                           name={props.to_concept_name}
+                           icon={
+                             <LocalOfferIcon
+                               fontSize='small'
+                                         style={{width: '10pt', color: DARKGRAY}}
+                             />
+                           } />;
   return (
-    <ResourceLabel
-      owner={props.to_source_owner} parent={props.to_source_name}
-      id={props.to_concept_code} name={props.to_concept_name}
-      icon={<LocalOfferIcon fontSize='small' style={{width: '10pt', color: DARKGRAY}}/>}
-    />
+    <React.Fragment>
+      {
+        props.to_concept_url ?
+        <Link to={props.to_concept_url}>{labelComponent}</Link>:
+        <span>{labelComponent}</span>
+      }
+    </React.Fragment>
   )
 }
 export default ToConceptLabel;
