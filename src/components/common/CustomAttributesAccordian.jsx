7c7202f4a3ff1a85f668f683984103141ffc393f
Sny <sny.aggarwal@gmail.com>
2020-12-01 20:44:41 +0530

    custom attributes accordian | handling nested attrs

--- src/components/common/CustomAttributesAccordian.jsx
+++ src/components/common/CustomAttributesAccordian.jsx
@@ -3,7 +3,7 @@ import {
   Accordion, AccordionSummary, AccordionDetails, Typography
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { map, isEmpty, isBoolean } from 'lodash';
+import { map, isEmpty, isBoolean, isArray, isObject, find } from 'lodash';
 
 const None = () => {
   return <div style={{margin: '5px', fontWeight: '300'}}>None</div>
@@ -12,6 +12,22 @@ const None = () => {
 const CustomAttributesAccordian = ({
   headingStyles, detailStyles, attributes
 }) => {
+
+  const shouldBeNested = value => {
+    return isArray(value) && Boolean(find(value, isObject))
+  }
+
+  const getNestedValueDom = value => {
+    return isObject(value) ?
+           <details>
+             <summary>{`${JSON.stringify(value).slice(0, 50)}...`}</summary>
+             <p>
+               <pre style={{fontSize: '12px'}}>{JSON.stringify(value, undefined, 2)}</pre>
+             </p>
+           </details> :
+           <code>{JSON.stringify(value)}</code>
+  }
+
   return (
     <Accordion defaultExpanded>
       <AccordionSummary
@@ -27,11 +43,21 @@ const CustomAttributesAccordian = ({
           None() :
           map(attributes, (value, name) => (
             <div className='col-md-12' style={{marginBottom: '5px'}} key={name}>
-              <div style={{fontWeight: '300'}} className='col-md-4 no-left-padding'>
+              <div style={{fontWeight: '300'}} className='col-md-3 no-left-padding'>
                 {name}
               </div>
-              <div className='col-md-8 no-right-padding ellipsis-text' style={{maxWidth: '100%'}}>
-                {isBoolean(value) ? value.toString() : value }
+              <div className='col-md-9 no-right-padding ellipsis-text' style={{maxWidth: '100%'}}>
+                {
+                  isBoolean(value) && value.toString()
+                }
+                {
+                  shouldBeNested(value) &&
+                  map(value, val => getNestedValueDom(val))
+                }
+                {
+                  !isBoolean(value) && !shouldBeNested(value) &&
+                  JSON.stringify(value)
+                }
               </div>
             </div>
           ))
