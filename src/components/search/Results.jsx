fed5db3fde7a5555c3a0b4bce057875efa5ee2dd
Sny <sny.aggarwal@gmail.com>
2020-12-03 15:24:22 +0530

    Row view disclaimer

--- src/components/search/Results.jsx
+++ src/components/search/Results.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Divider } from '@material-ui/core';
 import { Pagination } from '@material-ui/lab'
-import { map, startCase } from 'lodash';
+import { map, startCase, includes } from 'lodash';
 import Concept from '../concepts/Concept';
 import Mapping from '../mappings/Mapping';
 
 const Results = props => {
+  const { resource, results } = props;
+
   const getComponentFor = data => {
-    const { resource } = props;
     if(resource === 'concepts')
       return <Concept {...data} />;
     if(resource === 'mappings')
@@ -21,30 +22,34 @@ const Results = props => {
   return (
     <div className='col-sm-12 no-side-padding'>
       {
-        props.results.total ?
+        results.total ?
         <div className='col-sm-12 no-side-padding'>
           {
-            map(props.results.items, item => (
+            includes(['concepts', 'mappings'], resource) ?
+            map(results.items, item => (
               <div className='col-sm-12 no-side-padding' key={item.id}>
                 {getComponentFor(item)}
                 <Divider style={{width: '100%'}} />
               </div>
-            ))
+            )) :
+            <div className="col-sm-12 no-side-padding" style={{textAlign: 'center', margin: '10px 0'}}>
+              {`This view is not implemented yet for ${startCase(props.resource)}`}
+            </div>
           }
           <div className='col-sm-12 no-side-padding pagination' style={{textAlign: 'center'}}>
             <Pagination
               onChange={onPageChange}
-              count={props.results.pages}
+              count={results.pages}
               variant="outlined"
               shape="rounded"
               color="primary"
               showFirstButton
               showLastButton
-              page={props.results.pageNumber}
+              page={results.pageNumber}
             />
           </div>
         </div> :
-        <div style={{padding: '2px'}}>We found 0 {startCase(props.resource)}.</div>
+        <div style={{padding: '2px'}}>We found 0 {startCase(resource)}.</div>
       }
     </div>
   )
