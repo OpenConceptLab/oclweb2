056765639396df3cf6b57900a1684f4b9c40613a
Sny <sny.aggarwal@gmail.com>
2021-01-17 18:59:56 +0530

    collection/source SupportedLocales view (clubs default/supported)

---
+++ src/components/common/SupportedLocales.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Tooltip } from '@material-ui/core';
+import { map } from 'lodash';
+
+const SupportedLocales = ({default_locale, supported_locales}) => {
+  return (
+    <span>
+      <Tooltip title='Default Locale'>
+        <span><b>{default_locale}</b></span>
+      </Tooltip>
+      {
+        map(supported_locales, locale => {
+          if(locale !== default_locale) {
+            return (
+              <span key={locale}>, {locale}</span>
+            )
+          }
+        })
+      }
+    </span>
+  )
+}
+
+export default SupportedLocales;
