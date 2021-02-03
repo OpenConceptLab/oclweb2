d6d61154819b7c9c4fce20ef5d571800a8d99c43
Sny <sny.aggarwal@gmail.com>
2021-02-03 17:55:41 +0530

    concept row view click to open in new tab

--- src/components/concepts/Concept.jsx
+++ src/components/concepts/Concept.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import { LocalOffer as LocalOfferIcon } from '@material-ui/icons'
 import { merge, get } from 'lodash'
 import { DARKGRAY } from '../../common/constants';
@@ -8,10 +9,12 @@ import LastUpdatedOnLabel from '../common/LastUpdatedOnLabel';
 const Concept = props => {
   return (
     <div className='col-sm-12' style={merge({paddingTop: '10px', paddingLeft: 0, paddingRight: 0}, get(props, 'style', {}))}>
-      <ResourceLabel
-        owner={props.owner} parent={props.source} id={props.display_name} name={props.id}
-        icon={<LocalOfferIcon fontSize='small' style={{width: '10pt', color: DARKGRAY}}/>}
-      />
+      <Link to={props.url} style={{display: 'inline-block'}} target="_blank">
+        <ResourceLabel
+          owner={props.owner} parent={props.source} id={props.display_name} name={props.id}
+          icon={<LocalOfferIcon fontSize='small' style={{width: '10pt', color: DARKGRAY}}/>}
+        />
+      </Link>
       <div className='col-sm-12 no-side-padding resource-attributes'>
         <span className='resource-attr'>Class:</span>
         <span className='resource-value'>{props.concept_class},</span>
