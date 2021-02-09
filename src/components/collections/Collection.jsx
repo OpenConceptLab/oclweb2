936c39fc185519785f352c075bfa848461452292
Sny <sny.aggarwal@gmail.com>
2021-02-09 18:04:37 +0530

    Source/Collection list view | added summary

--- src/components/collections/Collection.jsx
+++ src/components/collections/Collection.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { Loyalty as LoyaltyIcon } from '@material-ui/icons'
-import { merge, get } from 'lodash'
+import { merge, get, isEmpty } from 'lodash'
 import { DARKGRAY } from '../../common/constants';
 import ResourceLabel from '../common/ResourceLabel';
 import LastUpdatedOnLabel from '../common/LastUpdatedOnLabel';
+import Summary from '../common/ConceptContainerSummaryHorizontal';
 
 const Collection = props => {
+  const hasSummary = !isEmpty(props.summary);
+  const mainClass = 'no-left-padding ' + hasSummary ? 'col-sm-9': 'col-sm-12';
+
   return (
     <div className='col-sm-12' style={merge({paddingTop: '10px', paddingLeft: 0, paddingRight: 0}, get(props, 'style', {}))}>
-      <Link to={props.url} style={{display: 'inline-block'}} target="_blank">
-        <ResourceLabel
-          owner={props.owner} id={props.id} name={props.name}
-          icon={<LoyaltyIcon fontSize='small' style={{width: '10pt', color: DARKGRAY}}/>}
-          colorClass="collection-bg"
-        />
-      </Link>
-      <div className='col-sm-12 no-side-padding resource-attributes'>
-        <span className='resource-attr'>Collection Type:</span>
-        <span className='resource-value'>{props.collection_type || 'None'}</span>
-        {
-          props.description &&
-          <React.Fragment>
-            <br/>
-            <span className='resource-value'>{props.description}</span>
-          </React.Fragment>
-        }
+      <div className={mainClass}>
+        <Link to={props.url} style={{display: 'inline-block'}} target="_blank">
+          <ResourceLabel
+            owner={props.owner} id={props.id} name={props.name}
+            icon={<LoyaltyIcon fontSize='small' style={{width: '10pt', color: DARKGRAY}}/>}
+            colorClass="collection-bg"
+          />
+        </Link>
+        <div className='col-sm-12 no-side-padding resource-attributes'>
+          <span className='resource-attr'>Collection Type:</span>
+          <span className='resource-value'>{props.collection_type || 'None'}</span>
+          {
+            props.description &&
+            <React.Fragment>
+              <br/>
+              <span className='resource-value'>{props.description}</span>
+            </React.Fragment>
+          }
+        </div>
+        <LastUpdatedOnLabel date={props.updated_on} />
       </div>
-      <LastUpdatedOnLabel date={props.updated_on} />
+      {
+        hasSummary &&
+        <Summary {...props} />
+      }
     </div>
   )
 }
---
+++ src/components/common/ConceptContainerSummaryHorizontal.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import {
+  LocalOffer as LocalOfferIcon,
+  Link as LinkIcon,
+  AccountTreeRounded as TreeIcon,
+} from '@material-ui/icons'
+import { Tooltip } from '@material-ui/core';
+
+const TAG_ICON_STYLES = {width: '12px', marginRight: '4px', marginTop: '2px'}
+
+const ConceptContainerSummaryHorizontal = props => {
+  return (
+    <div className="col-sm-3" style={{textAlign: 'right'}}>
+      <Link to={props.concepts_url} target='_blank'>
+        <Tooltip title='Concepts'>
+          <span className='flex-vertical-center' style={{paddingRight: '20px', fontSize: '14px',}}>
+            <LocalOfferIcon fontSize='small' style={TAG_ICON_STYLES} />
+            {props.summary.active_concepts}
+          </span>
+        </Tooltip>
+      </Link>
+      <Link to={props.mappings_url} target='_blank'>
+        <Tooltip title='Mappings'>
+          <span className='flex-vertical-center' style={{paddingRight: '20px', fontSize: '14px'}}>
+            <LinkIcon fontSize='small' style={TAG_ICON_STYLES} />
+            {props.summary.active_mappings}
+          </span>
+        </Tooltip>
+      </Link>
+      <Link to={props.versions_url} target='_blank'>
+        <Tooltip title='Versions'>
+          <span className='flex-vertical-center' style={{fontSize: '14px'}}>
+            <TreeIcon fontSize='small' style={TAG_ICON_STYLES} />
+            {props.summary.versions}
+          </span>
+        </Tooltip>
+      </Link>
+    </div>
+  )
+}
+
+export default ConceptContainerSummaryHorizontal;
