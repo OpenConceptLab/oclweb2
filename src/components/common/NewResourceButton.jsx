e4123b46fca6668c8a109bcb2a6f9a31f218c004
Sny <sny.aggarwal@gmail.com>
2021-02-17 12:15:59 +0530

    Feedback | new resources button are grouped

--- src/components/common/NewResourceButton.jsx
+++ src/components/common/NewResourceButton.jsx
@@ -1,27 +1,39 @@
 import React from 'react';
 import { startCase, map } from 'lodash';
-import { Button } from '@material-ui/core';
+import { Button, ButtonGroup, Menu, MenuItem, Tooltip } from '@material-ui/core';
 import {
-  Add as AddIcon,
+  Settings as SettingsIcon,
+  ArrowDropDown as DropDownIcon
 } from '@material-ui/icons'
 
 const NewResourceButton = ({resources, onClick}) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const toggleAnchorEl = event => setAnchorEl(prev => prev ? null : event.currentTarget)
+
+  const onItemClick = resource => {
+    onClick(resource)
+    toggleAnchorEl()
+  }
+
+  const formatResourceName = resource => resource.startsWith('edit') ? startCase(resource) : `Add ${startCase(resource)}`
+
   return (
     <React.Fragment>
-      {
-        map(resources, resource => (
-          <Button
-            key={resource}
-            style={{marginRight: '5px'}}
-            size="small"
-            color="primary"
-            variant="outlined"
-            startIcon={<AddIcon fontSize="inherit"/>}
-            onClick={() => onClick(resource)}>
-            {startCase(resource)}
-          </Button>
-        ))
-      }
+      <Tooltip title='Manage Org Content'>
+      <ButtonGroup color='primary' size='small' onClick={toggleAnchorEl}>
+        <Button><SettingsIcon /></Button>
+        <Button><DropDownIcon /></Button>
+      </ButtonGroup>
+      </Tooltip>
+      <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={toggleAnchorEl}>
+        {
+          map(resources, resource => (
+            <MenuItem key={resource} onClick={() => onItemClick(resource)}>
+              {formatResourceName(resource)}
+            </MenuItem>
+          ))
+        }
+      </Menu>
     </React.Fragment>
   )
 }
