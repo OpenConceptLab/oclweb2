1a43cd482335da09b84b72d8c29b3b1ffc692b4f
Sny <sny.aggarwal@gmail.com>
2021-02-18 16:42:02 +0530

    search results | list view | only source child are selectable

--- src/components/search/RowComponent.jsx
+++ src/components/search/RowComponent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Divider, Checkbox } from '@material-ui/core';
+import { includes } from 'lodash';
 import Concept from '../concepts/Concept';
 import Mapping from '../mappings/Mapping';
 import Source from '../sources/Source';
@@ -8,6 +9,8 @@ import Organization from '../orgs/Organization';
 import User from '../users/User';
 
 const RowComponent = ({resource, item, onSelect, viewFields}) => {
+  const isSourceChild = includes(['concepts', 'mappings'], resource);
+
   const getComponent = () => {
     if(resource === 'concepts')
       return <Concept {...item} viewFields={viewFields} style={{paddingLeft: '10px'}} />;
@@ -25,9 +28,12 @@ const RowComponent = ({resource, item, onSelect, viewFields}) => {
 
   return (
     <div className='col-sm-12 no-side-padding' key={item.uuid || item.id} style={{width: '100%'}}>
-      <div className='col-sm-1 no-left-padding' style={{textAlign: 'right', width: '2%'}}>
-        <Checkbox onChange={event => onSelect(event, item.url)} />
-      </div>
+      {
+        isSourceChild &&
+        <div className='col-sm-1 no-left-padding' style={{textAlign: 'right', width: '2%'}}>
+          <Checkbox onChange={event => onSelect(event, item.url)} />
+        </div>
+      }
       <div className='col-sm-11 no-right-padding'>
         {getComponent()}
       </div>
