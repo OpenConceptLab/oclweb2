60c8fcf99d111091c2c402cb453156a0a39d93a1
Sny <sny.aggarwal@gmail.com>
2021-03-02 10:24:40 +0530

    Concept/Mapping Home | add to collection control

--- src/components/concepts/ConceptHomeTabs.jsx
+++ src/components/concepts/ConceptHomeTabs.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Tabs, Tab } from '@material-ui/core';
+import { isLoggedIn } from '../../common/utils';
 import ConceptHomeDetails from './ConceptHomeDetails';
 import ConceptHomeMappings from './ConceptHomeMappings';
 import VersionList from '../common/VersionList';
+import AddToCollection from '../common/AddToCollection';
 
 const ConceptHomeTabs = props => {
   const { tab, concept, versions, currentURL, isVersionedObject } = props;
   const resourceRelativeURL = isVersionedObject ? concept.url : concept.version_url;
+  const isAuthenticated = isLoggedIn();
   return (
     <div className='col-md-12 sub-tab'>
-      <Tabs className='sub-tab-header' value={tab} aria-label="concept-home-tabs"  classes={{indicator: 'hidden'}}>
+      <Tabs className='sub-tab-header col-md-8 no-side-padding' value={tab} aria-label="concept-home-tabs"  classes={{indicator: 'hidden'}}>
         <Tab label="Details" component="a" href={`#${resourceRelativeURL}details/`} />
         <Tab label="Mappings" component="a" href={`#${resourceRelativeURL}mappings/`} />
         <Tab label="History" component="a" href={`#${resourceRelativeURL}history/`} />
       </Tabs>
-      <div className='sub-tab-container' style={{display: 'flex', height: 'auto'}}>
+      {
+        isAuthenticated &&
+        <div className='col-md-4 no-right-padding' style={{textAlign: 'right'}}>
+          <AddToCollection
+            variant='outlined' color='primary' size='small' references={[{...concept, url: resourceRelativeURL}]}
+          />
+        </div>
+      }
+      <div className='sub-tab-container' style={{display: 'flex', height: 'auto', width: '100%'}}>
         { tab === 0 && <ConceptHomeDetails concept={concept} currentURL={currentURL} /> }
         { tab === 1 && <ConceptHomeMappings concept={concept} /> }
         { tab === 2 && <VersionList versions={versions} resource='concept' /> }
--- src/components/mappings/MappingHomeTabs.jsx
+++ src/components/mappings/MappingHomeTabs.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Tabs, Tab } from '@material-ui/core';
+import { isLoggedIn } from '../../common/utils';
 import MappingHomeDetails from './MappingHomeDetails';
 import VersionList from '../common/VersionList';
+import AddToCollection from '../common/AddToCollection';
 
 const MappingHomeTabs = props => {
   const { tab, mapping, versions, isVersionedObject } = props;
   const resourceRelativeURL = isVersionedObject ? mapping.url : mapping.version_url;
+  const isAuthenticated = isLoggedIn();
   return (
     <div className='col-md-12 sub-tab'>
-      <Tabs className='sub-tab-header' value={tab} aria-label="mapping-home-tabs"  classes={{indicator: 'hidden'}}>
+      <Tabs className='sub-tab-header col-md-8 no-side-padding' value={tab} aria-label="mapping-home-tabs"  classes={{indicator: 'hidden'}}>
         <Tab label="Details" component="a" href={`#${resourceRelativeURL}details/`} />
         <Tab label="History" component="a" href={`#${resourceRelativeURL}history/`} />
       </Tabs>
-      <div className='sub-tab-container' style={{display: 'flex', height: 'auto'}}>
+      {
+        isAuthenticated &&
+        <div className='col-md-4 no-right-padding' style={{textAlign: 'right'}}>
+          <AddToCollection
+            variant='outlined' color='primary' size='small' references={[{...mapping, url: resourceRelativeURL}]}
+          />
+        </div>
+      }
+      <div className='sub-tab-container' style={{display: 'flex', height: 'auto', width: '100%'}}>
         { tab === 0 && <MappingHomeDetails mapping={mapping} /> }
         { tab === 1 && <VersionList versions={versions} resource='mapping' /> }
       </div>
