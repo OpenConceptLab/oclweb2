46b87d1fd648a58c20c5b94b92cf59035d02e16d
Sny <sny.aggarwal@gmail.com>
2021-03-24 22:15:03 +0530

    added styles injection

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import MomentUtils from '@date-io/moment';
 import { Route, HashRouter, Switch } from 'react-router-dom';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider, createMuiTheme, StylesProvider } from '@material-ui/core/styles';
 import alertifyjs from 'alertifyjs';
 import App from './components/app/App';
 import './index.scss';
@@ -38,14 +38,16 @@ const theme = createMuiTheme({
 
 ReactDOM.render(
   <HashRouter>
-    <MuiThemeProvider theme={theme}>
-      <MuiPickersUtilsProvider utils={MomentUtils}>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <App />
-        </Switch>
-      </MuiPickersUtilsProvider>
-    </MuiThemeProvider>
+    <StylesProvider injectFirst>
+      <MuiThemeProvider theme={theme}>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <App />
+          </Switch>
+        </MuiPickersUtilsProvider>
+      </MuiThemeProvider>
+    </StylesProvider>
   </HashRouter>,
   document.getElementById('root')
 );
