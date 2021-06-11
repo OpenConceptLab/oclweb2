5583706be2b2330e32563dc599250fb4f54b33af
Sny <sny.aggarwal@gmail.com>
2021-06-11 14:33:19 +0530

    Collapsible Header | default tooltip | light mode

--- src/components/common/CollapsibleDivider.jsx
+++ src/components/common/CollapsibleDivider.jsx
@@ -4,16 +4,17 @@ import { ArrowDropDown as DownIcon, ArrowDropUp as UpIcon } from '@material-ui/i
 import { BLACK } from '../../common/constants';
 const LIGHT_GRAY = 'rgba(0, 0, 0, 0.12)';
 
-const CollapsibleDivider = ({open, tooltip, onClick}) => {
+const CollapsibleDivider = ({open, tooltip, onClick, light}) => {
+  const expandColor = light ? LIGHT_GRAY : BLACK;
   return (
-    <div className='col-md-12 no-side-padding flex-vertical-center'>
-      <Divider style={{width: '48%', backgroundColor: open ? BLACK : LIGHT_GRAY}} />
-      <Tooltip title={tooltip} arrow>
-        <IconButton onClick={onClick} color={open ? 'secondary' : 'default'} size='small' style={{border: '1px solid', borderColor: open ? BLACK : LIGHT_GRAY}}>
+    <div className='col-md-12 no-side-padding flex-vertical-center' style={{justifyContent: 'center'}}>
+      <Divider style={{width: '48%', backgroundColor: open ? expandColor : LIGHT_GRAY}} />
+      <Tooltip title={tooltip || (open ? 'Collapse' : 'Exapnd')} arrow>
+        <IconButton onClick={onClick} color={open ? 'secondary' : 'default'} size='small' style={{border: '1px solid', borderColor: open ? expandColor : LIGHT_GRAY}}>
           {open ? <UpIcon fontSize='inherit' /> : <DownIcon fontSize='inherit' />}
         </IconButton>
       </Tooltip>
-      <Divider style={{width: '48%', backgroundColor: open ? BLACK : LIGHT_GRAY}} />
+      <Divider style={{width: '48%', backgroundColor: open ? expandColor : LIGHT_GRAY}} />
     </div>
 
   )
