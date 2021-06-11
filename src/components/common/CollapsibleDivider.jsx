6546425c900e5f3f9c6889e242f27cdd7e1a0e59
Sny <sny.aggarwal@gmail.com>
2021-06-11 17:14:52 +0530

    Collapsible divider to have hover effect

--- src/components/common/CollapsibleDivider.jsx
+++ src/components/common/CollapsibleDivider.jsx
@@ -7,7 +7,7 @@ const LIGHT_GRAY = 'rgba(0, 0, 0, 0.12)';
 const CollapsibleDivider = ({open, tooltip, onClick, light}) => {
   const expandColor = light ? LIGHT_GRAY : BLACK;
   return (
-    <div className='col-md-12 no-side-padding flex-vertical-center' style={{justifyContent: 'center'}}>
+    <div onClick={onClick} className='col-md-12 no-side-padding flex-vertical-center divider-highlight-hover' style={{justifyContent: 'center', cursor: 'pointer'}}>
       <Divider style={{width: '48%', backgroundColor: open ? expandColor : LIGHT_GRAY}} />
       <Tooltip title={tooltip || (open ? 'Collapse' : 'Exapnd')} arrow>
         <IconButton onClick={onClick} color={open ? 'secondary' : 'default'} size='small' style={{border: '1px solid', borderColor: open ? expandColor : LIGHT_GRAY}}>
