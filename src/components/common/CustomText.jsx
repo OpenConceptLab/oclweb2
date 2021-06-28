3b80f15174bf8a38f80a285a9017a204ae80be88
Sny <sny.aggarwal@gmail.com>
2021-06-28 11:14:56 +0530

    OpenConceptLab/ocl_issues#819 | Custom Text tab config | can specifiy file url to read from

--- src/components/common/CustomText.jsx
+++ src/components/common/CustomText.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import CustomMarkup from './CustomMarkup';
 import CustomMarkdown from './CustomMarkdown';
 
-const CustomText = ({title, value, format}) => {
+const CustomText = ({title, value, format, url}) => {
+  const [urlText, setURLText] = React.useState(null);
+  const [fetched, setFetched] = React.useState(false)
+  if(url && !urlText && !fetched) {
+    setFetched(true)
+    fetch(url).then(response => response.text()).then(text => setURLText(text))
+  }
   return (
     <div className='col-md-12 no-side-padding'>
       { title && <h3> { title } </h3> }
       { format === 'md' && <CustomMarkdown markdown={value} /> }
       { format === 'text' && <div> { value } </div> }
       { (!format || format === 'html') && <CustomMarkup markup={value} /> }
+      {
+        (!format || format === 'html') && urlText && <CustomMarkup markup={urlText} />
+      }
+      {
+        (format === 'md') && urlText && <CustomMarkdown markdown={urlText} />
+      }
+      {
+        (format === 'text') && urlText && <div> {urlText} </div>
+      }
     </div>
   );
 }
