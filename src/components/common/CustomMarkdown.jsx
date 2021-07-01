476db3e811bec728364a6806f5e66aa323bf61a0
Sny <sny.aggarwal@gmail.com>
2021-07-01 09:21:56 +0530

    OpenConceptLab/ocl_issues#825 | markdown to render table and other things better

--- src/components/common/CustomMarkdown.jsx
+++ src/components/common/CustomMarkdown.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
+import './CustomMarkdown.scss'
 
 const CustomMarkdown = ({title, markdown}) => {
   return (
-    <div className='col-md-12 no-side-padding'>
+    <div className='col-md-12 no-side-padding custom-markdown'>
       { title && <h3> { title } </h3> }
-      { markdown && <ReactMarkdown>{ markdown }</ReactMarkdown> }
+      {
+        markdown &&
+        <ReactMarkdown escapeHtml={false} remarkPlugins={[gfm]} children={markdown} />
+      }
     </div>
   );
 }
