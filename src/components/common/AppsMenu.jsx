ed3c39a89ce0f8c30be4564343998d1ce75baaca
Akhil Kala <akhilkala@pop-os.localdomain>
2021-08-31 15:10:37 +0530

    OpenConceptLab/ocl_issues#945 | Apps menu added

--- src/components/common/AppsMenu.jsx
+++ src/components/common/AppsMenu.jsx
@@ -4,7 +4,7 @@ import { Popper, ClickAwayListener, Tooltip, Grow, Paper, IconButton, Box, Typog
 import useToggle from '../../hooks/useToggle';
 import OpenMRSLogo from '../common/OpenMRSLogo';
 import { useLocation, useHistory } from 'react-router';
-import { BLUE, OPENMRS_URL } from '../../common/constants';
+import { OPENMRS_URL } from '../../common/constants';
 
 const AppsMenu = () => {
   const open = useToggle()
@@ -35,41 +35,39 @@ const AppsMenu = () => {
 
   return (
     <React.Fragment>
-        <Tooltip arrow title="Apps menu">
-          <IconButton style={{marginRight:"5px"}} touch='true' onClick={open.toggle} ref={anchorRef}>
-              <AppsIcon/>
-          </IconButton>
-        </Tooltip>
-        <Popper placement='bottom-end' open={open.value} anchorEl={anchorRef.current} role={undefined} transition disablePortal style={{zIndex: 1}}>
+      <Tooltip arrow title="Apps menu">
+        <IconButton className={open.value ? "app-menu-toggle selected" : "app-menu-toggle"} style={{marginRight:"5px"}} touch='true' onClick={open.toggle} ref={anchorRef}>
+          <AppsIcon/>
+        </IconButton>
+      </Tooltip>
+      <Popper placement='bottom-end' open={open.value} anchorEl={anchorRef.current} role={undefined} transition disablePortal style={{zIndex: 1}}>
         {({ TransitionProps }) => (
           <Grow
             {...TransitionProps}
             style={{
               transformOrigin:'right top',
             }}
-            >
-            <Paper style={{minWidth: '400px', minHeight: '150px' , border: '1px solid lightgray'}}>
+          >
+            <Paper className="app-menu flex-vertical-center">
               <ClickAwayListener onClickAway={handleClose}>
-                <Box py={5} display="flex" justifyContent="space-around" >
-                  <Box onClick={onTermBrowserClick} style={{cursor:"pointer"}} display="flex" flexDirection="column" alignItems="center">
+                <Box className="app-container" display="flex" justifyContent="space-around">
+                  <Box className={location.pathname !== "/imports" ? "app selected" : "app"} onClick={onTermBrowserClick} display="flex" flexDirection="column" alignItems="center">
                     <MetadataBrowserIcon fontSize="large"/>
                     <Typography style={{lineHeight:"1.2", marginTop:"15px"}} align="center" component="h6">
                       OCL <br/> TermBrowser
                     </Typography>
-                    { location.pathname !== "/imports" && <Box marginTop="10px" width="10px" height="10px" style={{background:BLUE}} borderRadius="50%"></Box>}
                   </Box>
-                  <Box onClick={onOpenMRSClick} style={{cursor:"pointer"}} display="flex" flexDirection="column" alignItems="center">
+                  <Box className="app" onClick={onOpenMRSClick} display="flex" flexDirection="column" alignItems="center">
                     <OpenMRSLogo style={{width:"30px"}} />
                     <Typography style={{lineHeight:"1.2", marginTop:"15px"}} align="center" component="h6">
                       OpenMRS <br/> Dictionary <br/> Manager
                     </Typography>
                   </Box>
-                  <Box onClick={onImportsClick} style={{cursor:"pointer"}} display="flex" flexDirection="column" alignItems="center">
+                  <Box className={location.pathname == "/imports" ? "app selected" : "app"} onClick={onImportsClick} display="flex" flexDirection="column" alignItems="center">
                     <ImportsIcon fontSize="large"/>
                     <Typography style={{lineHeight:"1.2", marginTop:"15px"}} align="center" component="h6">
                       Bulk <br/> Importer
                     </Typography>
-                    { location.pathname === "/imports" && <Box marginTop="10px" width="10px" height="10px" style={{background:BLUE}} borderRadius="50%"></Box>}
                   </Box>
                 </Box>
               </ClickAwayListener>
