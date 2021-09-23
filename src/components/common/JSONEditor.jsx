925491392d778c268c006d12793e872e5016cd6d
Akhil Kala <akhilkala@pop-os.localdomain>
2021-09-23 10:36:29 +0530

    json timer removed

--- src/components/common/JSONEditor.jsx
+++ src/components/common/JSONEditor.jsx
@@ -5,7 +5,7 @@ import locale    from 'react-json-editor-ajrm/locale/en';
 const JSONEditor = props => {
 
   return (
-    <JSONInput waitAfterKeyPress={2500} locale={locale} height='auto' width='100%' {...props} />
+    <JSONInput locale={locale} height='auto' width='100%' {...props} />
   )
 }
 
