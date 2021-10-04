80384cf19ffce8f79907e85fff01a966d44e259c
Sny <sny.aggarwal@gmail.com>
2021-10-04 21:37:50 +0530

    OpenConceptLab/ocl_issues#1017 | about tab | using h1 for heading

--- src/components/common/About.jsx
+++ src/components/common/About.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const About = ({id, about}) => {
   return (
     <div className='col-md-12 about-home-tab'>
-      <h3 style={{padding: '10px'}}>
+      <h1 style={{padding: '10px'}}>
         {`About ${id}`}
-      </h3>
+      </h1>
       {
         about ?
         <div className='col-md-12' dangerouslySetInnerHTML={{__html: about.replaceAll('href="/', 'href="/#/')}} /> :
