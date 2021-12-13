4e93646a0d84b894cd59f7033bf0cc683ade1e45
Sny <sny.aggarwal@gmail.com>
2021-12-13 19:57:44 +0530

    About Content | aligning margins and paddings

--- src/components/common/About.jsx
+++ src/components/common/About.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const About = ({id, about}) => {
   return (
     <div className='col-md-12 about-home-tab'>
-      <h1 style={{padding: '10px'}}>
+      <h1 style={{padding: '0px', marginBottom: 0}}>
         {`About ${id}`}
       </h1>
       {
         about ?
-        <div className='col-md-12' dangerouslySetInnerHTML={{__html: about.replaceAll('href="/', 'href="/#/')}} /> :
+        <div className='col-md-12 no-side-padding' dangerouslySetInnerHTML={{__html: about.replaceAll('href="/', 'href="/#/')}} /> :
         <p>No about entry</p>
       }
     </div>
