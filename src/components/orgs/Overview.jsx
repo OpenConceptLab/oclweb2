7f56d55add6b6f94d84ba90d2113b6ccf858ed4f
Sny <sny.aggarwal@gmail.com>
2021-12-20 10:25:21 +0530

    OpenConceptLab/ocl_issues#1133 | org overview | no pinned content yet message

--- src/components/orgs/Overview.jsx
+++ src/components/orgs/Overview.jsx
@@ -7,17 +7,23 @@ const Overview = ({ org, pins, onPinDelete, onPinOrderUpdate, canDeletePin, memb
   return (
     <div className='col-md-12 no-side-padding'>
       <div className='col-xs-9 no-side-padding'>
-      <Pins
-        pins={pins}
-        onDelete={onPinDelete}
-        canDelete={canDeletePin}
-        onOrderUpdate={onPinOrderUpdate}
-        style={{marginBottom: '5px', padding: '0 10px'}}
-      />
-      {
-        org.text &&
-        <div className='col-md-12' dangerouslySetInnerHTML={{__html: org.text.replaceAll('href="/', 'href="/#/')}} />
-      }
+        {
+          isEmpty(pins) ?
+          <h2 style={{textAlign: 'center'}}>
+            {`${org.id} doesn't have any pinned content yet.`}
+          </h2> :
+          <Pins
+            pins={pins}
+            onDelete={onPinDelete}
+            canDelete={canDeletePin}
+            onOrderUpdate={onPinOrderUpdate}
+            style={{marginBottom: '5px', padding: '0 10px'}}
+          />
+        }
+        {
+          org.text &&
+          <div className='col-md-12' dangerouslySetInnerHTML={{__html: org.text.replaceAll('href="/', 'href="/#/')}} />
+        }
       </div>
       {
         !isEmpty(members) &&
