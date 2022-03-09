02918ccb109fe69edc0ab3b5dd951e82419428df
Sny <sny.aggarwal@gmail.com>
2022-03-09 13:33:18 +0530

    OpenConceptLab/ocl_issues#1203 | Filters Button style

--- src/components/common/FilterButton.jsx
+++ src/components/common/FilterButton.jsx
@@ -4,16 +4,16 @@ import {
   FilterList as FilterListIcon,
 } from '@mui/icons-material';
 
-const FilterButton = ({tooltipTitle, label, count, disabled, onClick, size, minWidth}) => {
+const FilterButton = ({tooltipTitle, label, count, disabled, onClick, size, minWidth, isOpen}) => {
   const hasFilters = count && count > 0;
-  const color = hasFilters ? 'primary' : 'secondary';
+  const color = (hasFilters || isOpen) ? 'primary' : 'secondary';
   const buttonLabel = (label || 'Filters') + (hasFilters ? ` (${count})` : '');
 
   return (
     <Tooltip arrow title={tooltipTitle || 'Filter Results'}>
       <Chip
         onClick={onClick}
-        variant={hasFilters ? 'default' : 'outlined'}
+        variant={(hasFilters || isOpen) ? 'filled' : 'outlined'}
         color={color}
         icon={<FilterListIcon fontSize='small' />}
         label={buttonLabel}
