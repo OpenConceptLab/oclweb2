836f42febc3e17ab22e84d0dbc5af7747156da90
Sny <sny.aggarwal@gmail.com>
2022-05-11 14:04:57 +0530

    OpenConceptLab/ocl_issues#1292 | exclude/include reference indicator

--- src/components/common/ReferenceChip.jsx
+++ src/components/common/ReferenceChip.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Chip } from '@mui/material';
+import { Chip, Tooltip } from '@mui/material';
 import {
-  LocalOffer as LocalOfferIcon, Link as LinkIcon
+  LocalOffer as LocalOfferIcon, Link as LinkIcon,
 } from '@mui/icons-material';
 import { toFullAPIURL } from '../../common/utils';
 
@@ -15,12 +15,25 @@ const ReferenceChip = props => {
   else if(type && type.toLowerCase() === 'concepts')
     icon = <LocalOfferIcon fontSize='small' color='primary' />;
 
-  const chip = <Chip className='clickable' icon={icon} label={expression} variant='outlined' color='primary' style={{border: 'none'}} />
-
+  const chip = <Chip
+                 className='clickable'
+                 icon={icon}
+                 label={expression}
+                 variant='outlined'
+                 color='primary'
+                 style={{border: 'none'}}
+               />
   return (
+    <span>
       <a href={toFullAPIURL(props.uri)} target='_blank' rel='noopener noreferrer'>
         {chip}
       </a>
+      {
+        props.include ?
+          <Chip size='small' variant='outlined' color='success' label='include' /> :
+          <Chip size='small' variant='outlined' color='error' label='exclude' />
+      }
+    </span>
   )
 }
 
