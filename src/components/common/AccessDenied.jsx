126e751cb4376aed6c44d68b1985fe6de415c44c
Sny <sny.aggarwal@gmail.com>
2023-04-26 15:46:45 +0530

    OpenConceptLab/ocl_issues#1560 | fixing access denied signin button

--- src/components/common/AccessDenied.jsx
+++ src/components/common/AccessDenied.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { getLoginURL } from '../../common/utils'
+import { getLoginURL, isSSOEnabled } from '../../common/utils'
 import ErrorUI from './ErrorUI';
 
 const AccessDenied = () => {
-  const loginURL = getLoginURL(window.location.hash.replace('#', ''))
+  const loginURL = getLoginURL(isSSOEnabled() ? window.location.origin + '/' + window.location.hash : window.location.hash.replace('#', ''))
   return (
     <ErrorUI
       header='401'
