d540c716bdb57114ee65a31204bfc8d76dad4a1c
Sunny Aggarwal <sny.aggarwal@gmail.com>
2026-09-14 16:34:57 +0530

    OpenConceptLab/ocl_online#137 | Add env specific domains

--- src/services/GAService.js
+++ src/services/GAService.js
@@ -3,10 +3,23 @@ import ReactGA from 'react-ga4';
 import { startCase } from 'lodash';
 import { OCL_CLIENT } from '../common/constants';
 
-const LINKED_DOMAINS = ['openconceptlab.org', 'preview.openconceptlab.org', 'map.openconceptlab.org', 'app.v3.openconceptlab.org'];
 const SIGNUP_FLOW_PENDING_KEY = 'signup_flow_pending';
 
 const gaId = () => window.GA_ACCOUNT_ID || process.env.GA_ACCOUNT_ID;
+
+// The marketing site only exists in prod, at these two domains, regardless
+// of which env this app itself is running in. v3./map. mirror this app's
+// own env (e.g. app.qa. -> app.v3.qa., app.qa. -> map.qa.).
+const linkedDomains = () => {
+  const host = window.location.host;
+
+  return [
+    'openconceptlab.org',
+    'preview.openconceptlab.org',
+    host.replace('app.', 'app.v3.'),
+    host.replace('app.', 'map.'),
+  ];
+};
 
 const initialize = options => {
   /*eslint no-undef: 0*/
@@ -16,7 +29,7 @@ const initialize = options => {
 const GAService = {
   recordPageView() {
     // eslint-disable-next-line spellcheck/spell-checker
-    initialize({ gtagOptions: { linker: { domains: LINKED_DOMAINS } } });
+    initialize({ gtagOptions: { linker: { domains: linkedDomains() } } });
     ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.hash.split('?')[0] });
   },
 
